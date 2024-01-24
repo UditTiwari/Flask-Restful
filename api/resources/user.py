9dac1df853c9a173e977f04a10b69e49c9fd2a1b
UditTiwari <udittiwari1502@gmail.com>
2024-01-24 15:03:54 +0530

    blueprint restful

---
+++ api/resources/user.py
@@ -0,0 +1,44 @@
+from flask_restful import Resource,abort
+from users import users
+from flask import request
+
+
+class UserList(Resource):
+
+    def get(self):
+        return {"result":users}
+    
+    def post(self):
+        data = request.json
+        last_user_id = users[-1].get("id")
+        new_user = {"":last_user_id+1,**data}
+        users.append(new_user)
+
+        return {"msg":"User Created","user":new_user}
+    
+class UserResource(Resource):
+    def get(self,user_id):
+        user = next(filter(lambda u :u.get("id") == user_id,users),None)
+
+        if user is None:
+            abort(404)
+
+        return {"user":user}
+    
+    def put(self,user_id):
+        data = request.json
+
+        user = None
+
+        for i ,u in enumerate(users):
+            if u.get("id") == user_id:
+                users[i] = {**u,**data} #**data will overdie the **u
+                user = users[i]
+
+            if user is None:
+                abort(404)
+
+            return {"msg":"User is updated","user",user}
+        
+
+    def delete(self,user_id):
---
+++ users.py
@@ -0,0 +1,7 @@
+users = [
+    {"id":1,"name":"Udit Tiwari","email":"udit@Gmail.com","age":24},
+    {"id":2,"name":"Jiya Tiwari","email":"john@Gmail.com","age":14},
+    {"id":3,"name":"Swati Mishra","email":"john@Gmail.com","age":19},
+    {"id":4,"name":"Ravi Yadav","email":"john@Gmail.com","age":23},
+    {"id":5,"name":"Nidgi bhargava","email":"john@Gmail.com","age":23},
+]
