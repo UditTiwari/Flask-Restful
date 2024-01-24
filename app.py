89e1601ba9e95d56f7f302963e497a53e644ceed
UditTiwari <udittiwari1502@gmail.com>
2024-01-24 15:23:27 +0530

    enc exaple

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from api.views import blueprint
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 
 app = Flask(__name__)
 
 app.register_blueprint(blueprint=blueprint)
 
 
+debug = os.environ.get("FLASK_DEBUG")
+host = os.environ.get("FLASK_HOST","0.0.0.0")
+port = os.environ.get("FLASK_PORT",8000)
+
 if __name__=='__main__':
-    app.run(host="0.0.0.0",port=8000,debug=True)
+    app.run(host=host,port=port,debug=debug)
